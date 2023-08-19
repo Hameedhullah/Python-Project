ea75534e55a78dd5dbe728d441275b70858acc57
Hameedhullah <70830003+Hameedhullah@users.noreply.github.com>
2023-08-19 21:15:03 +0530

    Add files via upload

--- Mail valid.py
+++ Mail valid.py
@@ -1,8 +1,16 @@
 import re
-emailcheck = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
-useremail = input('Enter your Email - ')
 
-if re.search(emailcheck,useremail):
+# Regular expression pattern for basic email validation
+email_pattern = r"^[a-z]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+
+# Get user input for email
+user_email = input('Enter your Email - ')
+
+# Compile the regular expression with the IGNORECASE flag
+pattern = re.compile(email_pattern, re.IGNORECASE)
+
+# Check if the entered email matches the pattern
+if pattern.match(user_email):
     print('Correct Email')
 else:
     print('Wrong Email')
